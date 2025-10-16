965242e8694605fafbfc9e12cebabde944a7f4d4
Ali Gul <aligulphulpoto21@gmail.com>
2025-10-16 20:18:05 +0500

    Fixed template folder path for Render

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'),
+             static_folder=os.path.join(os.path.dirname(__file__), 'static'))
+
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
